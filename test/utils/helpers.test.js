7ea2be1c2b09863f84dd27c293ef1e37af998245
flpms <filipemelodasilva@gmail.com>
2016-04-25 14:53:20 -0300

    Ensure have properties

--- test/utils/helpers.test.js
+++ test/utils/helpers.test.js
@@ -5,6 +5,22 @@ const helpers = require('../../utils/helpers.js');
 const chai = require('chai');
 const assert = require('assert');
 
-describe('helpers test', function() {
+const expect = chai.expect;
 
+describe('helpers test', function() {
+    it('Expected helper be a object', function(){
+        expect(helpers).to.be.a('object');
+    });
+
+    it('Expected helper have a searchQuery', function(){
+        expect(helpers).to.have.property('searchQuery');
+    });
+
+    it('Expected helper have a generateOptions', function(){
+        expect(helpers).to.have.property('generateOptions');
+    });
+
+    it('Expected helper have a batchDetails', function(){
+        expect(helpers).to.have.property('batchDetails');
+    });
 });
