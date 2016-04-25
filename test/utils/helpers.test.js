287affc77a8521a4e55a7825a39780e06321f3a4
flpms <filipemelodasilva@gmail.com>
2016-04-25 14:47:05 -0300

    init write tests in mocha

---
+++ test/utils/helpers.test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const helpers = require('../../utils/helpers.js');
+
+const chai = require('chai');
+const assert = require('assert');
+
+describe('helpers test', function() {
+
+});
